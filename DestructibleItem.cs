0b9bbdcc4a5213df84c0c1d393b9282cd9cff68f
Paweł Kita <kitapawel@gmail.com>
2020-04-18 19:15:27 +0200

    Added scripts responsible for item destruction.

---
+++ DestructibleItem.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+    public class DestructibleItem : MonoBehaviour
+    {        
+        [SerializeField] GameObject brokenObject;
+        [SerializeField] AudioClip[] hitSounds;
+        [SerializeField] int structurePoints = 3;
+        int currentStructurePoints;
+        AudioSource audioSource;
+        bool isDestroyed = false;
+        public bool SetDestroyed(bool value)
+        {
+            if (value == true)
+            {
+                isDestroyed = true;
+            }
+            return isDestroyed;
+        }
+
+        void Start()
+        {
+            if (gameObject.GetComponent<AudioSource>() == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+            else
+            {
+                audioSource = GetComponent<AudioSource>();
+            }
+
+            currentStructurePoints = structurePoints;
+        }
+
+        void Update()
+        {
+            if (currentStructurePoints <= 0)
+            {
+                GameObject plank = Instantiate(brokenObject, gameObject.transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+
+        private void TakeDamage()
+        {
+            currentStructurePoints -= 1;
+        }
+
+        private void PlayHitSound()
+        {
+            var clip = hitSounds[Random.Range(0, hitSounds.Length)];
+            audioSource.PlayOneShot(clip);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Weapon")
+            {
+                TakeDamage();
+                PlayHitSound();
+                CameraShake.ShakeCamera(.1f, .5f); //TODO This line points to my CameraShake solution. You may use your own camera shake solution here or simply remove this line.
+            }
+        }
+
+    }
+
---
+++ DestructibleItemPart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+public class DestructibleItemPart : MonoBehaviour {
+
+    [SerializeField] AudioClip[] hitSounds;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        if (gameObject.GetComponent<AudioSource>() == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        } else
+        {
+            audioSource = GetComponent<AudioSource>();
+        }        
+        //TODO consider adding the same check as above for RigidBody component and possibly collider
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void PlayHitSound()
+    {
+        var clip = hitSounds[Random.Range(0, hitSounds.Length)];
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void OnCollisionEnter()
+    {
+        PlayHitSound();
+    }
+
+}
